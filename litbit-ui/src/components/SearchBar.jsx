0ca11c04f165ffb542ce0ccce79c720b07e12cbf
Tyler Roth <troth@CSMBP0004.local>
2017-08-20 10:59:16 -0400

    Pushing because any told me to

--- litbit-ui/src/components/SearchBar.jsx
+++ litbit-ui/src/components/SearchBar.jsx
@@ -14,170 +14,41 @@ import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
 import { MenuItem } from 'material-ui/Menu';
 import { withStyles } from 'material-ui/styles';
-// import match from 'autosuggest-highlight/match';
-// import parse from 'autosuggest-highlight/parse';
-//
-// const suggestions = [
-//     { label: 'Afghanistan' },
-//     { label: 'Aland Islands' },
-//     { label: 'Albania' },
-//     { label: 'Algeria' },
-//     { label: 'American Samoa' },
-//     { label: 'Andorra' },
-//     { label: 'Angola' },
-//     { label: 'Anguilla' },
-//     { label: 'Antarctica' },
-//     { label: 'Antigua and Barbuda' },
-//     { label: 'Argentina' },
-//     { label: 'Armenia' },
-//     { label: 'Aruba' },
-//     { label: 'Australia' },
-//     { label: 'Austria' },
-//     { label: 'Azerbaijan' },
-//     { label: 'Bahamas' },
-//     { label: 'Bahrain' },
-//     { label: 'Bangladesh' },
-//     { label: 'Barbados' },
-//     { label: 'Belarus' },
-//     { label: 'Belgium' },
-//     { label: 'Belize' },
-//     { label: 'Benin' },
-//     { label: 'Bermuda' },
-//     { label: 'Bhutan' },
-//     { label: 'Bolivia, Plurinational State of' },
-//     { label: 'Bonaire, Sint Eustatius and Saba' },
-//     { label: 'Bosnia and Herzegovina' },
-//     { label: 'Botswana' },
-//     { label: 'Bouvet Island' },
-//     { label: 'Brazil' },
-//     { label: 'British Indian Ocean Territory' },
-//     { label: 'Brunei Darussalam' },
-// ];
-//
-//
-// function renderInput(inputProps) {
-//     const { classes, home, value, ref, ...other } = inputProps;
-//
-//     return (
-//         <TextField
-//             autoFocus={home}
-//             value={value}
-//             inputRef={ref}
-//             InputProps={{
-//                 classes: {
-//                 },
-//                 ...other,
-//             }}
-//         />
-//     );
-// }
-//
-// function renderSuggestion(suggestion, { query, isHighlighted }) {
-//     const matches = match(suggestion.label, query);
-//     const parts = parse(suggestion.label, matches);
-//
-//     return (
-//         <MenuItem selected={isHighlighted} component="div">
-//             <div>
-//                 {parts.map((part, index) => {
-//                     return part.highlight
-//                         ? <span key={index} style={{ fontWeight: 300 }}>
-//                 {part.text}
-//               </span>
-//                         : <strong key={index} style={{ fontWeight: 500 }}>
-//                             {part.text}
-//                         </strong>;
-//                 })}
-//             </div>
-//         </MenuItem>
-//     );
-// }
-//
-// function renderSuggestionsContainer(options) {
-//     const { containerProps, children } = options;
-//
-//     return (
-//         <Paper {...containerProps} square>
-//             {children}
-//         </Paper>
-//     );
-// }
-//
-// function getSuggestionValue(suggestion) {
-//     return suggestion.label;
-// }
-//
-// function getSuggestions(value) {
-//     const inputValue = value.trim().toLowerCase();
-//     const inputLength = inputValue.length;
-//     let count = 0;
-//
-//     return inputLength === 0
-//         ? []
-//         : suggestions.filter(suggestion => {
-//             const keep =
-//                 count < 5 && suggestion.label.toLowerCase().slice(0, inputLength) === inputValue;
-//
-//             if (keep) {
-//                 count += 1;
-//             }
-//
-//             return keep;
-//         });
-// }
-//
-// const styles = theme => ({
-//     container: {
-//         flexGrow: 1,
-//         position: 'relative',
-//         height: 200,
-//     },
-//     suggestionsContainerOpen: {
-//         position: 'absolute',
-//         marginTop: theme.spacing.unit,
-//         marginBottom: theme.spacing.unit * 3,
-//         left: 0,
-//         right: 0,
-//     },
-//     suggestion: {
-//         display: 'block',
-//     },
-//     suggestionsList: {
-//         margin: 0,
-//         padding: 0,
-//         listStyleType: 'none',
-//     },
-//     textField: {
-//         width: '100%',
-//     },
-// });
-//
-// var classes;
+import axios from 'axios'
+
+function callServer(){
+
+    var requestBody = '{"data":{"ingredients":["cranberry juice","orange juice","vodka"]}}'
+    // var output = fetch('0.0.0.0:4567/drink', {
+    //     method: 'POST',
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json',
+    //     },
+    //     body: JSON.stringify({
+    //         firstParam: requestBody
+    //     })
+    // })
+    axios.get('http://localhost:4567/hello')
+        .then(function (response){
+            console.log(`TWRR >> resp=${response}`)
+        })
+        .catch(function(error){
+            console.log(`TWRR >> NONONONONO error=${error}`)
+        })
+    
+}
 
 export default class DrinkList extends React.Component {
 
-    // state = {
-    //     value: '',
-    //     suggestions: [],
-    // };
-    //
-    // handleSuggestionsFetchRequested = ({ value }) => {
-    //     this.setState({
-    //         suggestions: getSuggestions(value),
-    //     });
-    // };
-    //
-    // handleSuggestionsClearRequested = () => {
-    //     this.setState({
-    //         suggestions: [],
-    //     });
-    // };
-    //
-    // handleChange = (event, { newValue }) => {
-    //     this.setState({
-    //         value: newValue,
-    //     });
-    // };
+    constructor(props){
+        super(props);
+        this.handleClick = this.handleClick.bind(this);    
+    }
+
+    handleClick() {
+        callServer();
+    }
 
     render() {
 
@@ -195,7 +66,7 @@ export default class DrinkList extends React.Component {
                         />
                     </CardContent>
                     <CardActions>
-                        <Button  raised color="primary">Add</Button>
+                        <Button  raised color="primary" onClick={this.handleClick}>Add</Button>
                     </CardActions>
                 </Card>
             </Grid>
